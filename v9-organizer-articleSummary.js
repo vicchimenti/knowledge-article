e7949617e02052de1f753589984fae0997f085f4
Vic Chimenti <victor.chimenti@gmail.com>
2021-06-06 08:45:20 -0700

    rm list of tags

--- v9-organizer-articleSummary.js
+++ v9-organizer-articleSummary.js
@@ -27,7 +27,6 @@ try {
     var externalLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='External Link' output='normal' use-element='true' filename-element='External Link' modifiers='striptags,htmlentities' />");
     var fullTextLink = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, "<t4 type='content' name='Name' output='fulltext' use-element='true' filename-element='Name' modifiers='striptags,htmlentities' />");
     var lastModified = '<div class="lastModified" style="display:inline-block"><p>Last modified: <t4 type="meta" meta="last_modified" format="MMMM d, yyyy" /></p></div>'; 
-    var listOfTags = "";
     var titleLink = "";
     var thumbNailString = "";
   
@@ -70,7 +69,6 @@ try {
     document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, thumbNailString));
     document.write('<div class="articleSummary">');
     document.write('<div class="summary"><p>' + articleSummary + '</p></div>')
-    document.write(listOfTags);
     document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, lastModified));
     document.write('</div>'); // close articleSummary
     document.write(endingHTML);
